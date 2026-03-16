98191c40100786a48559d421a2d77d1503df379e
goutampatel18 <goutampatel154@gmail.com>
2026-03-16 21:20:36 +0530

    Add solution for Find the Smallest Divisor Given a Threshold

--- LeetCode/Find-the-Smallest-Divisor-Given-a-Threshold/Solution.cpp
+++ LeetCode/Find-the-Smallest-Divisor-Given-a-Threshold/Solution.cpp
@@ -1,48 +1,35 @@
 1class Solution {
 2public:
 3
-4    int finddays(vector<int>& weights, int cap ){
-5
-6        int days=1, load=0;
-7        for(int i=0; i<weights.size(); i++){
-8            if(weights[i]+load > cap){
-9                days++;
-10                load=weights[i];
-11            }
-12            else{
-13                load+=weights[i];
-14            }
-15        }
-16        return days;
-17    }
+4     int sumByD(vector<int>& arr, int div) {
+5        int sum = 0;
+6        for (int num : arr) {
+7            sum += ceil((double)num / div);
+8        }
+9        return sum;
+10    }
+11
+12    int smallestDivisor(vector<int>& arr, int target) {
+13        int n=arr.size();
+14
+15       
+16
+17        if (arr.size() > target) return -1;
 18
-19
-20    int shipWithinDays(vector<int>& weights, int days) {
-21        int n= weights.size(), sum=0, cap=0;
-22        int maxx= *max_element(weights.begin(), weights.end());
-23        int ans;
-24
-25
-26        for(int i=0; i<n; i++ ){
-27            sum+=weights[i];
-28        }
-29
-30        int low=maxx, high=sum;
+19        int low = 1;
+20        int high = *max_element(arr.begin(), arr.end());
+21
+22        // Binary search r
+23        while (low <= high) {
+24            int mid = (low + high) / 2;
+25            if (sumByD(arr, mid) <= target) {
+26                high = mid - 1;  
+27            } else {
+28                low = mid + 1;  
+29            }
+30        }
 31
-32        while(low<=high){
-33            int mid =low+(high-low)/2;
-34            int no_of_days = finddays(weights, mid);
-35            if(no_of_days<=days){
-36                high=mid-1;
-37                ans =mid;
-38                
-39            }
-40            else{
-41                low=mid+1;
-42                //  ans=mid;
-43            }
-44        }
-45
-46        return ans;
-47    }
-48};
+32        return low;
+33
+34    }
+35};
